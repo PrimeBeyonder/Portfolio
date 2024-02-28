ca832a61e865ffa967a3a46331879424b5b6fa8d
Andrew <hugomyat37@gmail.com>
2024-02-28 11:34:44 +0630

    services components added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import About from './components/about/About';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Service from './components/services/Service';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <main className="main">
         <Home />
         <About />
+        <Service />
       </main>
     </>
   )
