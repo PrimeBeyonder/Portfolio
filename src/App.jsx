1d54e8e79e26a5ec28fc41c14dbcca0dee1be924
Andrew <hugomyat37@gmail.com>
2024-02-29 15:43:33 +0630

    contact responsive fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Qulification from './components/qulification/Qulification';
 import Service from './components/services/Service';
 import Skill from './components/skills/Skill';
 import Testimonial from './components/Testimonials/Testimonial';
+import Contact from './components/contact/Contact';
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
         <Service />
         <Qulification />
         <Testimonial />
+        <Contact />
       </main>
     </>
   )
