6c277f14ca38c0e5cbc1c9ab046ef41976d43faf
Andrew <thum14252@gmail.com>
2024-05-25 21:17:07 +0630

    fixed

--- src/components/about/info.jsx
+++ src/components/about/info.jsx
@@ -6,12 +6,12 @@ const info = () => {
             <div className="about__box">
                 <i className="bx bx-award about__icon"></i>
                 <h3 className="about__title">Experience</h3>
-                <span className="about__subtitle">8 Years Working</span>
+                <span className="about__subtitle">- Months Working</span>
             </div>
             <div className="about__box">
                 <i className="bx bx-briefcase-alt about__icon"></i>
                 <h3 className="about__title">Completed</h3>
-                <span className="about__subtitle">48 + Projects</span>
+                <span className="about__subtitle">5+ Projects</span>
             </div>
             <div className="about__box">
                 <i className="bx bx-support about__icon"></i>
